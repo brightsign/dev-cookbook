6c032c0be23d6baf53a387f7ff8ac7365ded0c57
diegobrsign <dbenitez@brightsign.biz>
2024-01-26 12:23:10 -0600

    initial commit with two example apps

---
+++ cra-template-brightsign-app/template/src/App.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+const DeviceInfo = require('@brightsign/deviceinfo');
+const os = require('os');
+
+function App() {
+  const networkInterfaces = os.networkInterfaces() || {};
+  const [ipAddress, setIpAddress] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      Object.keys(networkInterfaces).forEach((interfaceName) => {
+        networkInterfaces[interfaceName].forEach((interfaceInfo) => {
+          if (interfaceInfo.family === 'IPv4') {
+            setIpAddress(`${interfaceName}: ${interfaceInfo.address} `);
+            console.log(
+              `Network Interface - ${interfaceName}: ${interfaceInfo.address}`
+            );
+          }
+        });
+      });
+
+    }, 5000)
+
+    return () => clearInterval(interval)
+  })
+
+  const deviceInfo = new DeviceInfo();
+  const { model, osVersion, serialNumber } = deviceInfo;
+
+  return (
+    <div className="App">
+      <h1>BrightSign React Web App Example</h1>
+      <div id="info">
+        <div id="ipAddress">
+          <span class="label">IP Address:</span>
+          <span class="value">{ipAddress || "loading..."}</span>
+        </div>
+        <div id="model">
+          <span class="label">Model:</span>
+          <span class="value">{model || "loading..."}</span>
+        </div>
+        <div id="osVersion">
+          <span class="label">OS Version:</span>
+          <span class="value">{osVersion || "loading..."}</span>
+        </div>
+        <div id="serialNumber">
+          <span class="label">Serial Number:</span>
+          <span class="value">{serialNumber || "loading..."}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cra-template-brightsign-app/template/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/index',
+  target: 'node',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: isProduction ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+              presets: ["@babel/preset-react"],
+          }
+        },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: ({ request }, callback) => {
+    if (/^@brightsign\//.test(request)) {
+      return callback(null, 'commonjs ' + request);
+    }
+    callback();
+  },
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
+};
