f3c019603be8d13660f02d02b228382d35c6e999
Ben Hastings <bhastings@brightsign.biz>
2024-02-01 16:01:42 -0800

    fix: change from port 80 to 8020
    * roHtmlWidget point to server

--- cra-template-brightsign-app/template/src/server/index.js
+++ cra-template-brightsign-app/template/src/server/index.js
@@ -3,12 +3,19 @@ const express = require("express");
 const path = __dirname + '../public';
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path));
 
 let latestText = "";
 
 // POST endpoint to receive updates
 app.post('/text', (req, res) => {
+    if (req === undefined) {
+      res.status(400).send("Bad request: no body provided.");
+    }
+    
+    console.log(`${req}`);
+    
     latestText = req.body.text;
 
     res.status(200).send("done");
@@ -18,5 +25,5 @@ app.get('/text', (req, res) => {
     res.status(200).send(latestText); 
 });
 
-const PORT = 80;
+const PORT = 8020;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
