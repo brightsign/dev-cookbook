04f01774a78d8f900c459d83565e0612d9a7e952
diegobrsign <dbenitez@brightsign.biz>
2024-03-27 08:56:12 -0600

    add manual mocks and test for brightsign app template

--- cra-template-brightsign-app/template/src/App.test.js
+++ cra-template-brightsign-app/template/src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
+
+describe('Device Info Mock', () => {
+  it('should return mocked device info', () => {
+    const DeviceInfo = require('@brightsign/deviceinfo');
+    const deviceInfo = new DeviceInfo();
+
+    expect(deviceInfo.model).toBe('MockModel');
+    expect(deviceInfo.osVersion).toBe('MockOSVersion');
+    expect(deviceInfo.serialNumber).toBe('MockSerialNumber');
+  });
+});
+
 
 test('renders expected body text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/BrightSign React Web App Example/i);
+  const linkElement = screen.getByText(/MockOSVersion/i);
   expect(linkElement).toBeInTheDocument();
 });
