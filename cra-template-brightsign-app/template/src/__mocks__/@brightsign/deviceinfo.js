3df66c7e8a92fabde649658d716348746093077c
diegobrsign <dbenitez@brightsign.biz>
2024-03-28 09:39:40 -0600

    add deviceuptime to deviceinfo mocks

--- cra-template-brightsign-app/template/src/__mocks__/@brightsign/deviceinfo.js
+++ cra-template-brightsign-app/template/src/__mocks__/@brightsign/deviceinfo.js
@@ -1,11 +1,15 @@
 // __mocks__/@brightsign/deviceinfo.js
 
+const startTime = Date.now() / 1000;
+
 class DeviceInfo {
     constructor() {
         this.model = "MockModel";
         this.osVersion = "MockOSVersion";
         this.serialNumber = "MockSerialNumber";
+        this.deviceUptime = Date.now() / 1000 - startTime;
     }
 }
 
+// Export the mock class
 module.exports = DeviceInfo;
--- example-node-simple-server/src/__mocks__/@brightsign/deviceinfo.js
+++ example-node-simple-server/src/__mocks__/@brightsign/deviceinfo.js
@@ -1,10 +1,13 @@
 // __mocks__/@brightsign/deviceinfo.js
 
+const startTime = Date.now() / 1000;
+
 class DeviceInfo {
     constructor() {
         this.model = "MockModel";
         this.osVersion = "MockOSVersion";
         this.serialNumber = "MockSerialNumber";
+        this.deviceUptime = Date.now() / 1000 - startTime;
     }
 }
 
