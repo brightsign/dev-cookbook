277f06482a0f69e297e2cb72dded36e362bea4ff
diegobrsign <dbenitez@brightsign.biz>
2024-05-10 10:20:23 -0600

    modifying comment

--- cra-template-brightsign-app/template/src/index.js
+++ cra-template-brightsign-app/template/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// add a comment
+// add a comment again
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
