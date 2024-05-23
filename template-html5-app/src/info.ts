8a7c7c2b9b3a9bdfa241e28fc22ca222bc24ee3f
jdmedlin1 <jmedlin@brightsign.biz>
2024-05-23 13:06:45 -0700

    rearranged files

---
+++ template-html5-app/src/info.ts
@@ -0,0 +1,38 @@
+import os from "os";
+
+import { DeviceInfo } from "./device-info";
+import { bsPlayer, BrightSignPlayer } from "./player";
+
+const displayCurrentNetwork = () => {
+    const networkInterfaces = os.networkInterfaces() || {};
+    let ipAddress = "";
+
+    Object.keys(networkInterfaces).forEach((interfaceName) => {
+        networkInterfaces[interfaceName]?.forEach((interfaceInfo) => {
+            if (interfaceInfo.family === "IPv4") {
+                ipAddress += `${interfaceName}: ${interfaceInfo.address} `;
+                console.log(
+                    `Network Interface - ${interfaceName}: ${interfaceInfo.address}`
+                );
+            }
+        });
+    });
+
+    document.querySelector("#ipAddress .value")!.textContent = ipAddress;
+};
+
+const displayDeviceInfo = async () => {
+    const player: BrightSignPlayer = await bsPlayer();
+    const deviceInfo: DeviceInfo = player.BSDeviceInfo;
+    const { model, osVersion, serialNumber } = deviceInfo;
+
+    document.querySelector("#model .value")!.textContent = model;
+    document.querySelector("#osVersion .value")!.textContent = osVersion;
+    document.querySelector("#serialNumber .value")!.textContent = serialNumber;
+
+    console.log(
+        `DeviceInfo - Model: ${model}, OS Version: ${osVersion}, Serial Number: ${serialNumber}`
+    );
+};
+
+export { displayCurrentNetwork, displayDeviceInfo };
