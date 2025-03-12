dead81822da635f1437d080e32619713ee9d11e2
jdmedlin1 <jmedlin@brightsign.biz>
2025-03-12 14:08:39 -0700

    omit unnecessary json stringification

--- examples/node-simple-server-example/src/app.js
+++ examples/node-simple-server-example/src/app.js
@@ -10,9 +10,8 @@ function app() {
         const server = http.createServer((req, res) => {
             // Serve device info on /api/device-info
             if (req.url === '/api/device-info') {
-                res.setHeader("Content-Type", "application/json");
-                const jsonResponse = JSON.stringify(di);
-                return res.end(jsonResponse);
+                res.setHeader("Content-Type", "application/json");;
+                return res.end(di);
             }
 
             // Serve static files from /storage/sd/
